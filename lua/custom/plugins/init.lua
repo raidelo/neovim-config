e0f504814786148f10af52ba12cc862d245b2fdf
raidelo <raidelosix@gmail.com>
2025-04-29 23:07:31 -0400

    feat: update options passed to `nvim-tree.lua` plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -9,4 +9,13 @@ return {
     "folke/which-key.nvim",
     lazy = false,
   },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    opts = function()
+      local opts = require "nvchad.configs.nvimtree"
+      opts.view.number = true
+      opts.view.relativenumber = true
+    end,
+  },
 }
